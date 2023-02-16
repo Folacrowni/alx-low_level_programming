964eba7d446404f2ffdad1f85feab0c1858e5bdb
Folashade Obada <folacrownie@gmail.com>
2023-02-16 15:35:21 -0800

    101quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - A program that print without pritg and put function
  * Description: Prints "and that piece of art is useful.." without puts
  * Return: 1 (success)
  */
 int main(void)
-{	
+{
 char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 long l = 59;
 long fd = 1;
