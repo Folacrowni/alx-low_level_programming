68206419b11748dc6074a0168c9591f3cb6c4d9c
Folashade Obada <folacrownie@gmail.com>
2023-02-21 15:42:24 -0800

    Task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
-#include"main.h"
-
+#include "main.h"
 /**
-* main-Entry point
-* Return: Always 0
+ * main - main function
+ * _putchar: print the alphabets putchar
+ * Return: always 0
 */
 
 int main(void)
-
 {
 _putchar('_');
 _putchar('p');
@@ -15,8 +14,7 @@ _putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
-_putchar('h');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
-
