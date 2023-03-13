0f4d0d00d250a644b90d913b1b1f3d4956bf1719
Folashade Obada <folacrownie@gmail.com>
2023-03-13 12:53:49 -0700

    updates

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,13 @@
 #include "main.h"
-
+#include<stdio.h>
 /**
- * _puts - prints a string
+ * _puts - print a string
  * @str: the string parameter
- * Return: nothing
+ * Return: void
  */
 
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
